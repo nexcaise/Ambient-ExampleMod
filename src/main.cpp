2f59424edee1d6e60a37e021a9af4b95d023ce93
alvinqid <github@alvinq.my.id>
2026-03-28 23:26:06 +0700

    fix or test!

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <Macro.h>
-#include <nise/stub.h>
-#include <pl/Logger.h>
+#include <android/log.h>
+
+#define LOG_TAG "ExampleMod"
+
+#define LOGI(fmt, ...) \
+__android_log_print(ANDROID_LOG_INFO, LOG_TAG, fmt, ##__VA_ARGS__)
 
 //this is constructor func (__attribute((constructor)))__);
 LoadAPI void onLoad() {
-    auto &logger = pl::log::Logger::getOrCreate("ExampleMod");
-    logger.info("Loading...");
-    logger.info("Loading Done");
+    LOGI("Loading...");
+    LOGI("Loading Done");
 }
