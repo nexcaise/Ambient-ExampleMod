24a5d9ed287277f148a7af8aad4505601b851b56
alvinqid <github@alvinq.my.id>
2026-03-29 14:08:41 +0700

    fix or test!

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,8 @@
-#include <Macro.h>
-#include <configAPI.h>
 #include <android/log.h>
-#include <main.h>
 
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, "ExampleMod", __VA_ARGS__)
 
 __attribute__((constructor))
 void onLoad() {
-    Config cfg("ExampleMod");
     LOGI("Loading...");
-
-    auto config = cfg.open("config.ini");
-
-    config.set("player.name", std::string("Zed"));
-    config.set("player.level", 10);
-
-    int level = config.get<int>("player.level", 0);
-    std::string name = config.get<std::string>("player.name", "unknown");
-
-    LOGI("Player Level: %d", level);
-    LOGI("Player Name: %s", name.c_str());
-    config.save();
-
-    LOGI("Loading Done");
 }
