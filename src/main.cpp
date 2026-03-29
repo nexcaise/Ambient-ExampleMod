b1c6fc8446ad3843aaa96fc4fb128dc794f75886
alvinqid <github@alvinq.my.id>
2026-03-29 08:25:16 +0700

    fix or test!

--- src/main.cpp
+++ src/main.cpp
@@ -5,25 +5,27 @@
 
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, "ExampleMod", __VA_ARGS__)
 
-//ConfigManager cfg("ExampleMod");
+ConfigManager cfg("ExampleMod");
 
 __attribute__((constructor))
 void onLoad() {
     LOGI("Loading...");
 //    LOGI("Package: %s", getAppPackage().c_str());
 
-/*    auto config = cfg.open("config.json");
+    cfg.load();
 
-    config.write("player.name", std::string("Zed"));
-    config.write("player.level", 10);
+    auto config = cfg.open("config.json");
 
-    int level = config.read<int>("player.level", 0);
-    std::string name = config.read<std::string>("player.name", "unknown");
+    config.set("player.name", std::string("Zed"));
+    config.set("player.level", 10);
+
+    int level = config.get<int>("player.level", 0);
+    std::string name = config.get<std::string>("player.name", "unknown");
 
     LOGI("Player Level: %d", level);
     LOGI("Player Name: %s", name.c_str());
 
     config.save();
-*/
+
     LOGI("Loading Done");
 }
